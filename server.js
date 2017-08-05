a141f63180066236b209b02767552813389e785f
SREEREKHAKP <30693943+SREEREKHAKP@users.noreply.github.com>
2017-08-05 18:15:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,16 +14,16 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 
-app.get('article-one'),function (req, res){
+app.get('/article-one'),function (req, res){
    res.send('Article one requested and will be served here'); 
 };
 
-app.get('article-two'),function (req, res){
+app.get('/article-two'),function (req, res){
    res.send('Article two requested and will be served here'); 
 };
 
 
-app.get('article-three'),function (req, res){
+app.get('/article-three'),function (req, res){
    res.send('Article three requested and will be served here'); 
 };
 
